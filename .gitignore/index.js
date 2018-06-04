67eecb07d4618a01925f88722c3e440ce5f59301
LeBl0ck <39920701+LeBl0ck@users.noreply.github.com>
2018-06-04 19:41:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,1 @@
-{
-  "name": "mrgrades",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "Le_Block",
-  "license": "ISC",
-  "dependencies": {
-    "discord.js": "^11.3.2"
-  }
-}
+
